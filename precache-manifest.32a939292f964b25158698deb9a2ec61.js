e4f11483ac2f30c954c52e8107579a89ca37e7d0
derensdima22 <d.berberov93@gmail.com>
2020-02-11 18:54:55 +0200

    Updates

---
+++ precache-manifest.32a939292f964b25158698deb9a2ec61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f361ae0cd844823f7d7ab5f4d7bd6017",
+    "url": "/homework1react/index.html"
+  },
+  {
+    "revision": "9b36d3555bf649227cdc",
+    "url": "/homework1react/static/css/main.05d9ceac.chunk.css"
+  },
+  {
+    "revision": "f740ba6b485bc26e7313",
+    "url": "/homework1react/static/js/2.2ccc0714.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/homework1react/static/js/2.2ccc0714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b36d3555bf649227cdc",
+    "url": "/homework1react/static/js/main.54814c54.chunk.js"
+  },
+  {
+    "revision": "38517358ada800a16010",
+    "url": "/homework1react/static/js/runtime-main.e858d7ca.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/homework1react/static/media/252.e45d9d1f.png"
+  }
+]);
